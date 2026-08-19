2c00ea586fdce6bd66d4c14316abb03c9241bb4f
Sali <salovecz@gmail.com>
2026-08-19 10:56:16 +0200

    LoginPage class filled

--- src/main/java/com/codecool/pages/LoginPage.java
+++ src/main/java/com/codecool/pages/LoginPage.java
@@ -1,4 +1,38 @@
 package com.codecool.pages;
 
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+
 public class LoginPage {
+
+    private final Page page;
+
+
+    private final Locator emailInput;
+    private final Locator passwordInput;
+    private final Locator loginButton;
+
+    public LoginPage(Page page) {
+    this.page = page;
+
+    emailInput = page.getByLabel("email");
+    passwordInput = page.getByLabel("password");
+
+    loginButton = page.getByRole(
+            AriaRole.BUTTON,
+            new Page.GetByRoleOptions()
+                    .setName("login")
+    );
+    }
+    public void navigate () {
+        page.navigate("http://localhost:5173/login");
+    }
+
+    public void login(String email, String password) {
+        emailInput.fill(email);
+        passwordInput.fill(password);
+        loginButton.click();
+    }
+
 }
